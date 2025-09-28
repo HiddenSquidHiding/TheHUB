e3972f0adba916e68b7e7fcce42b342c2c7a0fb1
HiddenSquidHiding <125598783+HiddenSquidHiding@users.noreply.github.com>
2025-09-29 00:32:31 +0100

    Create data_monsters.lua

---
+++ data_monsters.lua
@@ -0,0 +1,45 @@
+-- data_monsters.lua
+local M = {}
+
+M.toSahurModels = {
+  'To To To To To To To To To To To To To To To Sahur',
+  'To To To To To To To To To To To To To To Sahur',
+  'To To To To To To To To To To To To To Sahur',
+  'To To To To To To To To To To To To Sahur',
+  'To To To To To To To To To To To Sahur',
+  'To To To To To To To To To To Sahur',
+  'To To To To To To To To To Sahur',
+  'To To To To To To To To Sahur',
+  'To To To To To To To Sahur',
+}
+
+M.weatherEventModels = {
+  'Chicleteira','YONII','Coccodrillo Cheese','Chachechchachachachi',
+  'CanneIIoni Dragoni','GALALTUS MEDUS','Hipopotamino Supermakretino',
+  'Market Crate','BOSS','MALAME AMARALE BOSS','Malame Crate','Las Vaquitas',
+  'Trippi Troppi Troppa Trippa Boss','Los Tralaleritos',
+  'Trippi Troppi Troppa Trippa Boss2','Blueberrinni Octossini Boss',
+  'Blueberrinni Crate',
+}
+
+M.forcedMonsters = {
+  'Apple','Void Apple','Golden Apple','Golden Void Apple','Chick','Orange','Bull',
+  'Brrr Brr Patapim','Acorn','Lirili LariIa','Trash Bag','Red Panda','Tire','Rhino',
+  'Totem','Trippi Troppi Tropa Trippa','Box','Cacto Hipopootamo',
+  'La Vaca Saturno Saturnita','Cactus','Boar','Urn','Chameleon','Treasure',
+  'OrcaIero OrcaIa','Capuccino Assasino','Igloo','Tic Tac Tok Afammoc','Pinkaroo',
+  'Frozen Fish','Blizzy','Marshmallow','Aurox','SaIamino Pinguino','Toyboo','Spookit',
+  'Gear','Winding Key','Matteoo','Sea Urchin','Gorillo WatermellondriIIo','Axolotus',
+  'Oyster','Rhino Rhino Toasterino','Cog','Vault','Ecco CavaIIo Virtuoso',
+  'LeruIeruIeruIe','Helmet','PeIicini Pananini','Pile of Gold','Cocossini Mamma',
+  'Pillar','Espressona Signorra','Shimpanzini WatermeIonini Kingini','Ti Ti Ti Ti Sahur',
+  'Car','Satellite Dish','Racotucotulu','Electricity Pole','Trenostruzzo Turbo 3K',
+  'Nightmare Monster Sahur','Tombstone','TrikitrakateIas','Old Coffin',
+  'Tang Tang Tang Tang Kelentang','Rune','AIessio','Ketupat Kepat Kupat Prekupat',
+  'CentraIucci NucIearucci','Trash Pile','Radioactive Barrel','Big Pipe',
+  'Dar Dir Durr Dorr TeIur Dadar','Tirilikalika TiriIikalako','BambercIat Crococlat',
+  'TigrIIi Grapefrutunni','Brick','BuIbito Bandito Traktorito','Gangster Footeera',
+  'Screw','BOSS','Tiny Stars',
+}
+
+return M
